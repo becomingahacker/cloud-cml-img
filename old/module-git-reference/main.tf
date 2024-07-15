a62a6532fdca22bc835145be9990d75417b0331c
Chris McCoy <cmm@cisco.com>
2024-07-15 08:48:20 -0400

    Simplify everything and remove old files from EC2 image builder.

--- old/module-git-reference/main.tf
+++
@@ -1,9 +0,0 @@
-#
-# This file is part of Becoming a Hacker Foundations
-# Copyright (c) 2024, Cisco Systems, Inc.
-# All rights reserved.
-#
-
-data "external" "git_reference" {
-  program = ["bash", "${path.module}/git_reference.sh"]
-}
